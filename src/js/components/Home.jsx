692ea4d4bb7c4de4b12dae5b54acfca9013320f2
pedro-serrano-camblor <p.serranocamblor@gmail.com>
2025-06-16 12:22:28 +0000

    actualizado con añadir tareas funcionando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	//Declarar estados
+	const [newTask, setNewTask] = useState("")
+	const [tasks, setTasks] = useState([])
+	const [taskCounter, setTaskCounter] = useState(0)
+
+	//Funciones para manejar estados
+
+	function addTask(event) {
+
+		if (event.key === "Enter") {
+			//agregar tarea a la lista de tareas
+
+			setTasks(tasks.concat(newTask))
+			setNewTask("");
+
+		}
+		//event.preventDefault()
+
+	}
+	console.log(tasks);
+
+
+
+
+
+
+
 	return (
 		<div className="text-center">
-            
+			{/* Titulo todos */}
+			<h3 className="text-center mt-5 text-dark">todos</h3>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{/* contenedor central */}
+			<div className="container">
+
+				{/* Input */}
+				<input type="text" placeholder="texto aqui" value={newTask} onChange={(event) => setNewTask(event.target.value)} onKeyDown={addTask} />
+
+				{/* Lista */}
+
+				<ul>
+					{tasks.map((task, index) => <li key={index}>{task}</li>)}
+				</ul>
+
+
+
+				{/* Contador de elementos en la lista */}
+				<p>
+					Elementos en la lista
+				</p>
+			</div>
 		</div>
 	);
 };
